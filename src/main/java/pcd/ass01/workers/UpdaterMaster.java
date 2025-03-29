06e8d2454cf916de4dc8c94ecb4ecd49ec19243f
Agosta Alessandro <agosta.alessandro@studio.unibo.it>
2025-03-29 16:41:11 +0100

    first version

--- src/main/java/pcd/ass01/workers/UpdaterMaster.java
+++ src/main/java/pcd/ass01/workers/UpdaterMaster.java
@@ -10,42 +10,54 @@ import java.util.concurrent.Semaphore;
 
 public class UpdaterMaster {
 
-    private final int availableProcessors = Runtime.getRuntime().availableProcessors();
-
-    private final List<Worker> workers = new ArrayList<>();
-    private final MyCyclicBarrier startBarrier;
-    private final MyCyclicBarrier endBarrier;
+    private final List<Thread> workers = new ArrayList<>();
 
     public UpdaterMaster() {
 
-        this.startBarrier = new MyCyclicBarrier(availableProcessors + 1, "Start Barrier");
-        this.endBarrier = new MyCyclicBarrier(availableProcessors + 1);
-
-        var readToWriteBarrier = new MyCyclicBarrier(availableProcessors);
-
-        for (int index = 0; index < availableProcessors; index++) {
-            Worker worker = new Worker(index, this.startBarrier, this.endBarrier, readToWriteBarrier);
-            workers.add(worker);
-            worker.start();
-        }
     }
 
     public void update(BoidsModel model) {
-        log("li aggiorno");
-        this.workers.forEach(it -> it.setModel(model));
-        log("segnalo di partire ai worker [" + this.startBarrier.getQueuePosition() + "]");
-        try {
-            this.startBarrier.await(); //quando il main esegue questo await gli altri thread partono
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        workers.clear();
+
+        for(int i = 0; i < model.getBoids().size(); i++){
+            var boid = model.getBoids().get(i);
+            var worker = Thread.ofVirtual().unstarted(() ->{
+                boid.readNearbyBoids(model);
+            });
+            worker.start();
+
+            workers.add(worker);
         }
 
-        try {
-            log("aspetto i worker " + this.endBarrier.getQueuePosition());
-            this.endBarrier.await(); //quando gli altri thread finiscono libero il main
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        workers.forEach(it -> {
+            try {
+                it.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        workers.clear();
+
+        for(int i = 0; i < model.getBoids().size(); i++){
+            var boid = model.getBoids().get(i);
+            var worker = Thread.ofVirtual().unstarted(() ->{
+                boid.updateVelocity(model);
+                boid.updatePos(model);
+            });
+            worker.start();
+
+            workers.add(worker);
         }
+
+
+        workers.forEach(it -> {
+            try {
+                it.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     private synchronized void log(String msg) {
