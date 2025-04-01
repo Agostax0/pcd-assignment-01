65c1958a8f9a446c34cb2c5f612893c979dcce36
Agosta Alessandro <agosta.alessandro@studio.unibo.it>
2025-04-01 11:41:49 +0200

    cleanup for performance measuring

--- src/main/java/pcd/ass01/BoidsSimulator.java
+++ src/main/java/pcd/ass01/BoidsSimulator.java
@@ -7,57 +7,22 @@ import java.util.Optional;
 public class BoidsSimulator {
 
     private BoidsModel model;
-    private Optional<BoidsView> view;
-    
-    private static final int FRAMERATE = 60;
-    private int framerate;
 
     private final UpdaterMaster updaterMaster;
 
-    public BoidsSimulator(BoidsModel model) {
-        this.model = model;
-        view = Optional.empty();
-        updaterMaster = new UpdaterMaster();
-    }
+    private long startTime;
 
-    public void attachView(BoidsView view) {
-    	this.view = !(this.model.jpf) ? Optional.of(view) : Optional.empty();
+    public BoidsSimulator(BoidsModel model) {
+        startTime = System.currentTimeMillis();
+        this.model = model;
+        updaterMaster = new UpdaterMaster();
     }
       
     public void runSimulation() {
-    	while (true) {
-
-            if(!model.isModelPaused()){
-
-                var t0 = System.currentTimeMillis();
-
+        var iteration = 0;
+    	while (iteration++ < 1500) {
                 updaterMaster.update(model);
-
-
-
-                if (view.isPresent()) {
-                    view.get().update(framerate);
-                    var framratePeriod = 1000/FRAMERATE;
-
-                    var t1 = System.currentTimeMillis();
-                    var dtElapsed = t1 - t0;
-
-                    System.out.println("dtElapsed: " + dtElapsed);
-
-                    if (dtElapsed < framratePeriod) {
-                        try {
-                            Thread.sleep(framratePeriod - dtElapsed);
-                        } catch (Exception ex) {}
-                        framerate = FRAMERATE;
-                    } else {
-                        framerate = (int) (1000/dtElapsed);
-                    }
-                }
-            }
     	}
-    }
-
-    private synchronized void log(String msg){
-
+        System.out.println(System.currentTimeMillis() - startTime);
     }
 }
