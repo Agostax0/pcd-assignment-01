fd410db04a6fd9e4a16a6f5d90439d57ce4ba45e
Agosta Alessandro <agosta.alessandro@studio.unibo.it>
2025-04-01 12:28:12 +0200

    removed slowing synchronized blocks

--- src/main/java/pcd/ass01/BoidsModel.java
+++ src/main/java/pcd/ass01/BoidsModel.java
@@ -42,84 +42,75 @@ public class BoidsModel {
     private void initBoids(int number){
         boids = new ArrayList<>();
         for (int i = 0; i < number; i++) {
-            P2d pos;
-            V2d vel;
-            if(jpf){
-                double fakeRandom = (double) (i % 100) / 100;
-                pos = new P2d(-width/2 + fakeRandom/2 * width, -height/2 + fakeRandom/3 * height);
-                vel = new V2d(fakeRandom/4 * maxSpeed/2 - maxSpeed/4, fakeRandom/5 * maxSpeed/2 - maxSpeed/4);
-            }else{
-                pos = new P2d(-width/2 + Math.random() * width, -height/2 + Math.random() * height);
-                vel = new V2d(Math.random() * maxSpeed/2 - maxSpeed/4, Math.random() * maxSpeed/2 - maxSpeed/4);
-            }
+            P2d pos = new P2d(-width/2 + Math.random() * width, -height/2 + Math.random() * height);
+            V2d vel = new V2d(Math.random() * maxSpeed/2 - maxSpeed/4, Math.random() * maxSpeed/2 - maxSpeed/4);
             boids.add(new Boid(pos, vel));
-
         }
     }
 
-    public synchronized void setBoidsNumber(int value){initBoids(value);}
+    public void setBoidsNumber(int value){initBoids(value);}
     
-    public synchronized List<Boid> getBoids(){
+    public List<Boid> getBoids(){
     	return boids;
     }
     
-    public synchronized double getMinX() {
+    public double getMinX() {
     	return -width/2;
     }
 
-    public synchronized double getMaxX() {
+    public double getMaxX() {
     	return width/2;
     }
 
-    public synchronized double getMinY() {
+    public double getMinY() {
     	return -height/2;
     }
 
-    public synchronized double getMaxY() {
+    public double getMaxY() {
     	return height/2;
     }
     
-    public synchronized double getWidth() {
+    public double getWidth() {
     	return width;
     }
  
-    public synchronized double getHeight() {
+    public double getHeight() {
     	return height;
     }
 
-    public synchronized void setSeparationWeight(double value) {
+    public void setSeparationWeight(double value) {
     	this.separationWeight = value;
     }
 
-    public synchronized void setAlignmentWeight(double value) {
+    public void setAlignmentWeight(double value) {
     	this.alignmentWeight = value;
     }
 
-    public synchronized void setCohesionWeight(double value) {
+    public void setCohesionWeight(double value) {
     	this.cohesionWeight = value;
     }
 
-    public synchronized double getSeparationWeight() {
+    public double getSeparationWeight() {
     	return separationWeight;
     }
 
-    public synchronized double getCohesionWeight() {
+    public double getCohesionWeight() {
     	return cohesionWeight;
     }
 
-    public synchronized double getAlignmentWeight() {
+    public double getAlignmentWeight() {
     	return alignmentWeight;
     }
     
-    public synchronized double getMaxSpeed() {
+    public double getMaxSpeed() {
     	return maxSpeed;
     }
 
-    public synchronized double getAvoidRadius() {
+    public double getAvoidRadius() {
     	return avoidRadius;
     }
 
-    public synchronized double getPerceptionRadius() {
+    public double getPerceptionRadius() {
     	return perceptionRadius;
     }
 }
