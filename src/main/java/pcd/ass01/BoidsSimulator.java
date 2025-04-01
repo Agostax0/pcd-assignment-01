cd58d2ecafd7b09db2407514929b9424a282ec2e
Agosta Alessandro <agosta.alessandro@studio.unibo.it>
2025-04-01 11:52:09 +0200

    removed view

--- src/main/java/pcd/ass01/BoidsSimulator.java
+++ src/main/java/pcd/ass01/BoidsSimulator.java
@@ -1,65 +1,29 @@
 package pcd.ass01;
 
-import java.util.Optional;
 
 public class BoidsSimulator {
 
     private BoidsModel model;
-    private Optional<BoidsView> view;
-    
-    private static final int FRAMERATE = 25;
-    private int framerate;
-    
+
+    private long startTime;
+
     public BoidsSimulator(BoidsModel model) {
+        startTime = System.currentTimeMillis();
         this.model = model;
-        view = Optional.empty();
     }
 
-    public void attachView(BoidsView view) {
-    	this.view = !(this.model.jpf) ? Optional.of(view) : Optional.empty();
-    }
-      
     public void runSimulation() {
-    	while (true) {
-            var t0 = System.currentTimeMillis();
+        var iteration = 0;
+    	while (iteration++ < 1000) {
     		var boids = model.getBoids();
-    		/*
-    		for (Boid boid : boids) {
-                boid.update(model);
-            }
-            */
-    		
-    		/* 
-    		 * Improved correctness: first update velocities...
-    		 */
     		for (Boid boid : boids) {
                 boid.updateVelocity(model);
             }
-
-    		/* 
-    		 * ..then update positions
-    		 */
     		for (Boid boid : boids) {
                 boid.updatePos(model);
             }
-
-            
-    		if (view.isPresent()) {
-            	view.get().update(framerate);
-            	var t1 = System.currentTimeMillis();
-                var dtElapsed = t1 - t0;
-                var framratePeriod = 1000/FRAMERATE;
-                
-                if (dtElapsed < framratePeriod) {		
-                	try {
-                		Thread.sleep(framratePeriod - dtElapsed);
-                	} catch (Exception ex) {}
-                	framerate = FRAMERATE;
-                } else {
-                	framerate = (int) (1000/dtElapsed);
-                }
-    		}
-            
     	}
+
+        System.out.println(System.currentTimeMillis() - startTime);
     }
 }
