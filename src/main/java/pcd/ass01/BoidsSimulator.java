4fcaa3026f5e0d4128b4394081442626c0f6ca8d
Agosta Alessandro <agosta.alessandro@studio.unibo.it>
2025-04-07 15:54:33 +0200

    start-stop button

--- src/main/java/pcd/ass01/BoidsSimulator.java
+++ src/main/java/pcd/ass01/BoidsSimulator.java
@@ -26,32 +26,24 @@ public class BoidsSimulator {
       
     public void runSimulation() {
     	while (true) {
-
+            var t0 = System.currentTimeMillis();
             if(!model.isModelPaused()){
-
-                var t0 = System.currentTimeMillis();
-
                 updaterMaster.update(model);
+            }
+            if (view.isPresent()) {
+                view.get().update(framerate);
+                var framratePeriod = 1000/FRAMERATE;
 
+                var t1 = System.currentTimeMillis();
+                var dtElapsed = t1 - t0;
 
-
-                if (view.isPresent()) {
-                    view.get().update(framerate);
-                    var framratePeriod = 1000/FRAMERATE;
-
-                    var t1 = System.currentTimeMillis();
-                    var dtElapsed = t1 - t0;
-
-                    System.out.println("dtElapsed: " + dtElapsed);
-
-                    if (dtElapsed < framratePeriod) {
-                        try {
-                            Thread.sleep(framratePeriod - dtElapsed);
-                        } catch (Exception ex) {}
-                        framerate = FRAMERATE;
-                    } else {
-                        framerate = (int) (1000/dtElapsed);
-                    }
+                if (dtElapsed < framratePeriod) {
+                    try {
+                        Thread.sleep(framratePeriod - dtElapsed);
+                    } catch (Exception ex) {}
+                    framerate = FRAMERATE;
+                } else {
+                    framerate = (int) (1000/dtElapsed);
                 }
             }
     	}
